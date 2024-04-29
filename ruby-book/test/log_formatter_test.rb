2c447ddbf5a25174ae05088f3bfdcf11c2938f53
Takafumi Takemura <takfmr@gmail.com>
2024-04-29 20:19:34 +0900

    Chapter 11: Log Formatter

---
+++ ruby-book/test/log_formatter_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative '../lib/log_formatter'
+
+class LogFormatterTest < Minitest::Test
+  def test_format_log
+    text = LogFormatter.format_log
+    lines = text.lines(chomp: true)
+    assert_equal '[OK] request_id=1, path=/products/1', lines[0]
+    assert_equal '[ERROR] request_id=2, path=/wp-login.php, status=404, error=Not found', lines[1]
+    assert_equal '[WARN] request_id=3, path=/products, duration=1023.8', lines[2]
+  end
+end
